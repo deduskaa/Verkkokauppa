175715b1e3261b9e6a19c61023b204d502f845a4
deduskaa <jenny.tran@metropolia.fi>
2015-05-07 13:04:20 +0300

    lisätty trolli

--- lol.php
+++ lol.php
@@ -127,71 +127,9 @@ require_once('login.php');
 		</div>
 	  </div>
 	</nav>
-		<div id="myCarousel" class="carousel slide" data-ride="carousel">
-		  <!-- Indicators -->
-		  <ol class="carousel-indicators">
-			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-			<li data-target="#myCarousel" data-slide-to="1"></li>
-			<li data-target="#myCarousel" data-slide-to="2"></li>
-			<li data-target="#myCarousel" data-slide-to="3"></li>
-		  </ol>
-
-		  <!-- Wrapper for slides -->
-		  <div class="carousel-inner" role="listbox">
-			<div class="item active">
-			  <img src="http://www.mashinie.com/wp-content/uploads/2014/06/Play-any-Pokemon-Game-on-Android-Devices-using-this-Trick.jpg" alt="Chania">
-			</div>
-
-			<div class="item">
-			  <img src="http://www.mashinie.com/wp-content/uploads/2014/06/Play-any-Pokemon-Game-on-Android-Devices-using-this-Trick.jpg" alt="Chania">
-			</div>
-
-			<div class="item">
-			  <img src="http://www.mashinie.com/wp-content/uploads/2014/06/Play-any-Pokemon-Game-on-Android-Devices-using-this-Trick.jpg" alt="Flower">
-			</div>
-
-			<div class="item">
-			  <img src="http://www.mashinie.com/wp-content/uploads/2014/06/Play-any-Pokemon-Game-on-Android-Devices-using-this-Trick.jpg" alt="Flower">
-			</div>
-		  </div>
-
-		  <!-- Left and right controls -->
-		  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-			<span class="sr-only">Previous</span>
-		  </a>
-		  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-			<span class="sr-only">Next</span>
-		  </a>
-		</div>
     <div class="neighborhood-guides">
         <div class="container">
-            <h2>Typetypetype</h2>
-            <p>THE most popular pokemons that you will want to buy</p>
-            <div class="row">
-                <div class = "col-md-4">
-                    <div>
-                        <img id='frontPagepic' src="https://lh6.googleusercontent.com/-F3XMnJb0YsU/USlbnOls2YI/AAAAAAAADFY/1OVC0IOnvR4/w800-h800/sticker,375x360.png">
-                    </div>
-                      <div>
-                        <img id='frontPagepic' src="http://wordofgame.com/wp-content/uploads/2014/07/pokemon-water-type-symbol-mjz5sgae.png">
-                    </div>
-                </div>
-                <div class = "col-md-4">
-                    <div>
-                        <img id='frontPagepic' src="http://firetypepokemon.weebly.com/uploads/3/2/0/5/32050987/160983187.png">
-                    </div>
-                      <div>
-                        <img id='frontPagepic' src="http://cdn.playbuzz.com/cdn/4868eddb-fff5-41ba-8482-8436cdccf810/20f6c5af-1281-4327-988d-9b19999100a3.png">
-                    </div>
-                </div>
-                <div class = "col-md-4">
-                    <div>
-                        <img src="http://i.imgur.com/Ls6gd.jpg">
-                    </div>
-                </div>
-            </div>
+			<iframe width="1000" height="500" src="https://www.youtube.com/embed/LbenCTAwyco?rel=0&autoplay=1" frameborder="10" allowfullscreen></iframe>
         </div>
     </div>
 	
